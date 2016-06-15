9cb6232778952e66d1a40368aa89ec3c77a2b4c2
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2016-06-15 12:13:19 -0700

    fix the pony email issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,35 +11,35 @@ class SessionsController < ApplicationController
    flash[:info] = "a notice has been sent to your email for records " 
    redirect_to root_url
 
-    Pony.mail(to:   user.email,
-              from: "amircmpt@gmail.com",
-               subject:  "Thanks for the signin dear " ,
-              via: :smtp,
-              via_options: {
-              address: "smtp.gmail.com",
-              port: "587",
-              enable_starttls_auto: true,
-              user_name: "answerawesome",
-             password: "Sup3r$ecret",
-              authentication: :plain,
-              domain: "gmail.com"
-         	   }
-             )
+    # Pony.mail(to:   user.email,
+    #           from: "amircmpt@gmail.com",
+    #            subject:  "Thanks for the signin dear " ,
+    #           via: :smtp,
+    #           via_options: {
+    #           address: "smtp.gmail.com",
+    #           port: "587",
+    #           enable_starttls_auto: true,
+    #           user_name: "answerawesome",
+    #          password: "Sup3r$ecret",
+    #           authentication: :plain,
+    #           domain: "gmail.com"
+    #      	   }
+    #          )
     
-       Pony.mail(to:  "anabaei@sfu.ca",
-              from: "amircmpt@gmail.com",
-               subject:  "Thanks for the signin dear ",
-              via: :smtp,
-              via_options: {
-              address: "smtp.gmail.com",
-              port: "587",
-              enable_starttls_auto: true,
-              user_name: "answerawesome",
-             password: "Sup3r$ecret",
-              authentication: :plain,
-              domain: "gmail.com"
-                   }
-             )
+    #    Pony.mail(to:  "anabaei@sfu.ca",
+    #           from: "amircmpt@gmail.com",
+    #            subject:  "Thanks for the signin dear ",
+    #           via: :smtp,
+    #           via_options: {
+    #           address: "smtp.gmail.com",
+    #           port: "587",
+    #           enable_starttls_auto: true,
+    #           user_name: "answerawesome",
+    #          password: "Sup3r$ecret",
+    #           authentication: :plain,
+    #           domain: "gmail.com"
+    #                }
+    #          )
    else
         flash[:error] = "Invalid email/password"
         redirect_to root_url
